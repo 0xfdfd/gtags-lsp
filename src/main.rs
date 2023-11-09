70b6b416f9fa8b957b5aa3e2099ffc0e1acf3a48
qgymib <qgymib@gmail.com>
2023-11-09 20:22:48 +0800

    setup basic structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
-fn main() {
-    println!("Hello, world!");
+mod method;
+
+#[derive(Debug)]
+struct Runtime {
+    /// Program name.
+    prog_name: String,
+
+    /// Program version.
+    prog_version: String,
+
+    /// Workspace folder list.
+    workspace_folders: Vec<tower_lsp::lsp_types::WorkspaceFolder>,
+}
+
+#[derive(Debug)]
+pub struct TagsLspBackend {
+    client: tower_lsp::Client,
+    rt: tokio::sync::Mutex<Runtime>,
+}
+
+#[tower_lsp::async_trait]
+impl tower_lsp::LanguageServer for TagsLspBackend {
+    async fn initialize(
+        &self,
+        params: tower_lsp::lsp_types::InitializeParams,
+    ) -> tower_lsp::jsonrpc::Result<tower_lsp::lsp_types::InitializeResult> {
+        return method::initialize::do_initialize(self, params).await;
+    }
+
+    async fn initialized(&self, _: tower_lsp::lsp_types::InitializedParams) {
+        self.client
+            .log_message(
+                tower_lsp::lsp_types::MessageType::INFO,
+                "server initialized!",
+            )
+            .await;
+    }
+
+    async fn shutdown(&self) -> tower_lsp::jsonrpc::Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    const PROG_NAME: &str = env!("CARGO_PKG_NAME");
+    const PROG_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+    let rt = tokio::sync::Mutex::new(Runtime {
+        prog_name: PROG_NAME.to_string(),
+        prog_version: PROG_VERSION.to_string(),
+        workspace_folders: Vec::new(),
+    });
+
+    let (service, socket) = tower_lsp::LspService::new(|client| TagsLspBackend { client, rt });
+
+    tower_lsp::Server::new(stdin, stdout, socket)
+        .serve(service)
+        .await;
 }
---
+++ src/method/initialize.rs
@@ -0,0 +1,108 @@
+pub async fn do_initialize(
+    backend: &crate::TagsLspBackend,
+    params: tower_lsp::lsp_types::InitializeParams,
+) -> tower_lsp::jsonrpc::Result<tower_lsp::lsp_types::InitializeResult> {
+    let mut rt = backend.rt.lock().await;
+    copy_workspace_folder(&mut rt, &params);
+
+    Ok(tower_lsp::lsp_types::InitializeResult {
+        server_info: Some(tower_lsp::lsp_types::ServerInfo {
+            name: rt.prog_name.clone(),
+            version: Some(rt.prog_version.clone()),
+        }),
+        capabilities: get_server_capacity(),
+        ..Default::default()
+    })
+}
+
+/// Safe workspace folders from client initialize params.
+///
+/// # Arguments
+///
+/// + `dst` - A mut reference to Runtime.
+/// + `src` - Reference to InitializeParams
+fn copy_workspace_folder(dst: &mut crate::Runtime, src: &tower_lsp::lsp_types::InitializeParams) {
+    match &src.root_uri {
+        Some(value) => dst
+            .workspace_folders
+            .push(tower_lsp::lsp_types::WorkspaceFolder {
+                name: String::from(""),
+                uri: value.clone(),
+            }),
+        None => (),
+    };
+
+    match &src.workspace_folders {
+        Some(value) => dst.workspace_folders = value.clone(),
+        None => (),
+    };
+}
+
+/// Get server capacity
+fn get_server_capacity() -> tower_lsp::lsp_types::ServerCapabilities {
+    return tower_lsp::lsp_types::ServerCapabilities {
+        position_encoding: Some(tower_lsp::lsp_types::PositionEncodingKind::UTF8),
+        text_document_sync: Some(tower_lsp::lsp_types::TextDocumentSyncCapability::Kind(
+            tower_lsp::lsp_types::TextDocumentSyncKind::FULL,
+        )),
+        definition_provider: Some(tower_lsp::lsp_types::OneOf::Right(
+            tower_lsp::lsp_types::DefinitionOptions {
+                work_done_progress_options: tower_lsp::lsp_types::WorkDoneProgressOptions {
+                    work_done_progress: Some(true),
+                },
+            },
+        )),
+        type_definition_provider: Some(
+            tower_lsp::lsp_types::TypeDefinitionProviderCapability::Simple(true),
+        ),
+        implementation_provider: Some(
+            tower_lsp::lsp_types::ImplementationProviderCapability::Simple(true),
+        ),
+        references_provider: Some(tower_lsp::lsp_types::OneOf::Right(
+            tower_lsp::lsp_types::ReferencesOptions {
+                work_done_progress_options: tower_lsp::lsp_types::WorkDoneProgressOptions {
+                    work_done_progress: Some(true),
+                },
+            },
+        )),
+        workspace_symbol_provider: Some(tower_lsp::lsp_types::OneOf::Right(
+            tower_lsp::lsp_types::WorkspaceSymbolOptions {
+                work_done_progress_options: tower_lsp::lsp_types::WorkDoneProgressOptions {
+                    work_done_progress: Some(true),
+                },
+                resolve_provider: Some(false),
+            },
+        )),
+        declaration_provider: Some(tower_lsp::lsp_types::DeclarationCapability::Simple(true)),
+        workspace: Some(tower_lsp::lsp_types::WorkspaceServerCapabilities {
+            workspace_folders: Some(tower_lsp::lsp_types::WorkspaceFoldersServerCapabilities {
+                supported: Some(true),
+                change_notifications: Some(tower_lsp::lsp_types::OneOf::Left(false)),
+            }),
+            file_operations: Some(
+                tower_lsp::lsp_types::WorkspaceFileOperationsServerCapabilities {
+                    did_rename: Some(tower_lsp::lsp_types::FileOperationRegistrationOptions {
+                        filters: vec![tower_lsp::lsp_types::FileOperationFilter {
+                            scheme: Option::None,
+                            pattern: tower_lsp::lsp_types::FileOperationPattern {
+                                glob: String::from("*"),
+                                ..Default::default()
+                            },
+                        }],
+                    }),
+                    did_delete: Some(tower_lsp::lsp_types::FileOperationRegistrationOptions {
+                        filters: vec![tower_lsp::lsp_types::FileOperationFilter {
+                            scheme: Option::None,
+                            pattern: tower_lsp::lsp_types::FileOperationPattern {
+                                glob: String::from("*"),
+                                ..Default::default()
+                            },
+                        }],
+                    }),
+                    ..Default::default()
+                },
+            ),
+        }),
+        ..tower_lsp::lsp_types::ServerCapabilities::default()
+    };
+}
